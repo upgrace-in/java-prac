3b9d4fc66b1e86ef221f1398f06f669c3ccda536
upgrace-in <thedesiretree47@gmail.com>
2024-04-29 11:09:01 +0530

    hair-oops

---
+++ OOP/Concepts.java
@@ -0,0 +1,28 @@
+/**
+ * Encapsulation
+ */
+
+public class Person{
+    private String name;
+
+    // Getter
+    public String getName(){
+        return name;
+    }
+
+    // Setter
+    public void setName(String newName){
+        this.name = newName;
+    }
+}
+
+public class Concepts{
+    public static void main(String[] args){
+        Person Obj = new Person();
+        // Obj.name = "HARI"; // error
+        // System.out.println(Obj.getName()); // error bcuz name is private
+
+        Obj.setName("HARI");
+        System.out.println(Obj.getName());
+    }
+}
