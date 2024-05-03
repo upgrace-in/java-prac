7643a10be5b393180b30f67421ea649983c82cb4
upgrace-in <thedesiretree47@gmail.com>
2024-05-03 20:05:44 +0530

    hari

---
+++ advance/Prac6.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class Prac6{
+    public static void main(String[] args){
+        LinkedList<String> devs = new LinkedList<String>();
+        devs.add("HH RNSM");
+        devs.add("HH LNSM");
+        devs.add("HH JPSM");
+        devs.add("HH GKSM");
+        System.out.println(devs);
+    }
+}
