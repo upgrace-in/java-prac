afe1d69faab5b7873bafd6bd90814b6a454681bd
upgrace-in <thedesiretree47@gmail.com>
2024-08-29 11:31:27 +0530

    final keyword

---
+++ telusko/Final.java
@@ -0,0 +1,22 @@
+// final keyword works for variable, class, methods
+
+final class A {
+    public void show(){
+        System.out.println("A is shown!");
+    }
+}
+
+// now we can't extend A because that is a constant class
+class B extends A {
+    
+}
+
+public class Final {
+    public static void main(String[] args){
+        final float PI = 3.14f;
+        // we can't change here as the variable is declared as final
+        PI = 2.4f;
+    }   
+}
+
+// same goes with methods
